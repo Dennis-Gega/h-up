526b1a16236cfb300e96efacd2884a7f72dafab8
dennis gega <dennis.gega@uconn.edu>
2025-08-11 14:59:16 -0400

    temporary fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
-import Title from "@/components/ui/title";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
         <div className="flex items-center justify-center min-h-screen w-full">
             <div className="flex flex-col items-center">
-                <Title />
                 <div className="flex flex-col flex-wrap gap-4 justify-center my-10">
                     <Button asChild>
                         <Link href="/game/animals">animals</Link>
